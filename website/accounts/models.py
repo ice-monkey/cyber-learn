65f1a2deee483616341744f1ace1509afaf60925
ColdBanana <magn751j@stud.kea.dk>
2021-10-08 14:21:25 +0200

    New Backup

--- website/accounts/models.py
+++ website/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+	handle = models.CharField(max_length=200, null=True)
+	name = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)	
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
