dd0f26f0c05dd0270090f536255a0bb42f317394
Lea Gemzæe <leax7914@stud.kea.dk>
2021-12-11 13:06:54 +0100

    redirect to dashboard when user is already logged in

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -56,6 +56,10 @@ def user_dash(request):
 def loginPage(request):
     if 'next' in request.GET:
         messages.add_message(request, messages.INFO, 'You must be signed in to view this page')
+    
+    if request.user is not None:
+        return redirect('/')
+
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
